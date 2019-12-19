40a9ac644d2ea892eaf592f16039ef14242b22d9
Amit <eluminous_se42@eluminoustechnologies.com>
2019-12-19 11:52:10 +0530

    update contribution report to add total column at bottom

--- resources/views/admin/reports/contribution.blade.php
+++ resources/views/admin/reports/contribution.blade.php
@@ -61,6 +61,23 @@
                 </thead>
                 <tbody>
                 </tbody>
+                <tfoot align="right">
+                    <tr>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </tfoot>
             </table>
         </div>
     </div>
