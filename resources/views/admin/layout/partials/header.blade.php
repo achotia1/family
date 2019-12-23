a31e1cef183d7857c16616c76ffc97f0e05f5435
Amit <eluminous_se42@eluminoustechnologies.com>
2019-12-23 12:22:09 +0530

    update changes for title

--- resources/views/admin/layout/partials/header.blade.php
+++ resources/views/admin/layout/partials/header.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>@yield('title') | {{ config('constants.SITENAME') }}</title>
+  <title>@yield('title') | @if(!empty($company)) {{ str_replace(" ","",$company->name) }}Store  @endif</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <meta name="admin-path" content="{{ url('/admin') }}">
--- resources/views/admin/layout/web/header.blade.php
+++ resources/views/admin/layout/web/header.blade.php
@@ -1,9 +1,9 @@
- <!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>@yield('title') | Orchid Store</title>
+  <title>@yield('title') | @if(!empty($company)) {{ str_replace(" ","",$company->name) }}Store  @endif</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport') }}">
   <!-- Bootstrap 3.3.7 -->
