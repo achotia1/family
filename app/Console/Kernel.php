936bcb2324bbc4acf2473de48da2ba7c43e38884
nitinmahajaneluminous <nitin_mahajan@eluminoustechnologies.com>
2020-01-16 18:02:44 +0530

    Raw material report

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,7 +33,7 @@ class Kernel extends ConsoleKernel
         /*$schedule->command('openingbalance:day')
                   ->daily(12);*/
         $schedule->command('openingbalance:day')
-                  ->dailyAt('12:30');
+                  ->dailyAt('18:15');
         
     }
 
